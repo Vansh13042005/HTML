51a6a9910134959104bdf39ba11935306762d020
Vanshpatel5244 <patelvansh564738@gmail.com>
2025-05-30 11:08:31 +0530

    Create App.js

---
+++ assignment/React/jsx/src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+const jsxintro=()=>{
+  const topic="jsx"
+
+return(
+  <div>
+    <h1>welcome to {topic}</h1>
+    <p>{topic}short for JavaScript XML, is a syntax extension for JavaScript that enables developers to write HTML-like code directly within their JavaScript files</p>
+  </div>
+);
+};
+export default jsxintro;
